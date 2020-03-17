2273fa2d7f1d54427a3e24fefcf23d2bed775f21
Arthur <poubelle@arthur-maronian.fr>
2020-03-17 11:04:05 +0100

    Key fixes 2

--- src/fr/arthur/Main.java
+++ src/fr/arthur/Main.java
@@ -11,7 +11,7 @@ import net.dv8tion.jda.api.exceptions.RateLimitedException;
 
 public class Main {
 	public static void main(String[] args) throws LoginException, IllegalArgumentException, InterruptedException, RateLimitedException{
-	 JDA jda = new JDABuilder(AccountType.BOT).setToken("Njg3MzAyNzMyODU5NTcyMjI0.XnCfPw.A0mqiIBjF_Yk03sq9u60HVU5JhE").build();
+	 JDA jda = new JDABuilder(AccountType.BOT).setToken("Njg3MzAyNzMyODU5NTcyMjI0.XnCgfw.f6aT9MYH5SykEyrVPUg1wh7rBz0").build();
 	 Users.deser();
 	 Punishments.deser();
 	 jda.addEventListener(new Listener());
