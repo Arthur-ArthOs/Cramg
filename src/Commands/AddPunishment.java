a82256d40e9be124cc8c55cd301c23c5212a2d59
Arthur <poubelle@arthur-maronian.fr>
2020-03-17 12:48:40 +0100

    Global fixes for prod

--- src/Commands/AddPunishment.java
+++ src/Commands/AddPunishment.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import DB.Punishments;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class AddPunishment {
@@ -8,8 +9,14 @@ public class AddPunishment {
 		String reason = event.getMessage().getContentDisplay().replaceAll("\\*punish", "");
 		String reason2 = reason.replaceAll(args[1], "");
 		String reason3 = reason2.replaceAll(args[2], "");
-		System.out.println(reason3);
 		Punishments.add(args[2], reason3, args[1]);
+	    EmbedBuilder builder = new EmbedBuilder();
+	    builder.setAuthor("Cramg | Maison de Gestion : Bureau de l'Interieur", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setThumbnail("https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setFooter("By Cramg", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setTitle("Punition", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setDescription("L'utilisateur a bien été puni ! :white_check_mark:");
+	    event.getChannel().sendMessage(builder.build()).queue();
 		Punishments.ser();
 	}
 }
--- src/Commands/RemovePunishment.java
+++ src/Commands/RemovePunishment.java
@@ -1,11 +1,19 @@
 package Commands;
 
 import DB.Punishments;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class RemovePunishment {
 	public static void execute(String args[], MessageReceivedEvent event) {
 		Punishments.remove(Integer.parseInt(args[1]));
+	    EmbedBuilder builder = new EmbedBuilder();
+	    builder.setAuthor("Cramg | Maison de Gestion : Bureau de l'Interieur", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setThumbnail("https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setFooter("By Cramg", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setTitle("Dé-punition", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	    builder.setDescription("L'utilisateur a bien été dé-puni ! :white_check_mark:");
+	    event.getChannel().sendMessage(builder.build()).queue();
 		Punishments.ser();
 	}
 }
