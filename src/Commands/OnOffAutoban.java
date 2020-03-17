8c3cf41f0e7bd39d1be6a2cb7a169fa0100d14db
Arthur <poubelle@arthur-maronian.fr>
2020-03-17 11:39:42 +0100

    Autoban display

--- src/Commands/OnOffAutoban.java
+++ src/Commands/OnOffAutoban.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import DB.Autobans;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class OnOffAutoban {
@@ -8,8 +9,20 @@ public class OnOffAutoban {
 		if(event.getMember().getId().equalsIgnoreCase("185073797345312770")) {
 		  if(args[1].equalsIgnoreCase("on")) {
 			  Autobans.add(event.getGuild().getId());
+			  EmbedBuilder builder = new EmbedBuilder();
+	          builder.setAuthor("Cramg | Maison de Gestion : Bureau de l'Interieur", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setDescription("Autoban activé ! :white_check_mark_");
+	          builder.setThumbnail("https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setFooter("By Cramg", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setTitle("Autoban", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
 		  }else {
 			  Autobans.remove(event.getGuild().getId());
+			  EmbedBuilder builder = new EmbedBuilder();
+	          builder.setAuthor("Cramg | Maison de Gestion : Bureau de l'Interieur", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setDescription("Autoban désactivé ! :white_check_mark_");
+	          builder.setThumbnail("https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setFooter("By Cramg", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
+	          builder.setTitle("Autoban", "https://i.ibb.co/wW5xnwW/Sans-titre.png");
 		}
 	  }
    }
