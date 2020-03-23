dbaf7f986786079359f287311f9272c66e775373
Arthur <poubelle@arthur-maronian.fr>
2020-03-23 11:54:12 +0100

    Spring update

--- src/Commands/View.java
+++ src/Commands/View.java
@@ -13,8 +13,7 @@ public class View {
 	  for (Users user : Users.getArray()) {
 		  for (Member member : event.getGuild().getMembers()) {
 			  if(member.getUser().equals(event.getJDA().getUserById(user.getId()))) {
-			  System.out.print("cool");
-			  if(event.getJDA().getUserById(user.getId()).getAsMention().equals(args[1])) {
+			  if(event.getJDA().getUserById(user.getId()).getName().equalsIgnoreCase(args[1].toString())) {
 				  System.out.println("lol");
 			  }
 			  }
