8daed2b1fe5e05db37408112f20f89078c83d94a
Episcobus <poubelle@arthur-maronian.fr>
2020-04-18 19:16:20 +0200

    Update Main.java

--- src/fr/arthur/Main.java
+++ src/fr/arthur/Main.java
@@ -12,7 +12,7 @@ import net.dv8tion.jda.api.exceptions.RateLimitedException;
 
 public class Main {
 	public static void main(String[] args) throws LoginException, IllegalArgumentException, InterruptedException, RateLimitedException{
-	 JDA jda = new JDABuilder(AccountType.BOT).setToken("Njg3MzAyNzMyODU5NTcyMjI0.Xn99yQ.GbOPVhdW_g6VnQCP-iOCvUa8zWA").build();
+	 JDA jda = new JDABuilder(AccountType.BOT).setToken("****").build();
 	 Users.deser();
 	 Punishments.deser();
 	 Autobans.deser();
